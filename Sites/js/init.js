e399e920b6842a711f5d4759295e63d0a6b4fdcc
Gabriel Ferreira da silva <43484906+oo7gabriel@users.noreply.github.com>
2020-03-12 00:36:52 -0300

    Site de teste para estudo e treino
    Desenvolvimento de um site imaginário para estudo

---
+++ Sites/js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+});
